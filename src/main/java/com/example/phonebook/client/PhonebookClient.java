712f27dbf39cfd056ad51f84197448107603039d
Filiposu <togrul125@gmail.com>
2022-03-06 07:30:14 -0800

    Created frontend application

---
+++ src/main/java/com/example/phonebook/client/PhonebookClient.java
@@ -0,0 +1,33 @@
+package com.example.phonebook.client;
+
+import com.example.phonebook.dto.UserEntity;
+import com.example.phonebook.dto.UserOperation;
+import java.util.List;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@FeignClient(value = "phonebook-api", url = "${endpoints.phonebook-api}")
+public interface PhonebookClient {
+
+    @GetMapping(value = "/status",
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    List<UserEntity> getStatus( @RequestHeader(value = "Host") String hostName);
+
+    @GetMapping(value = "/user/list",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    List<UserEntity> getAllUsers( @RequestHeader(value = "Host") String hostName);
+
+    @PostMapping(value = "/user/add",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    UserOperation postUser(@RequestBody UserEntity userEntity,  @RequestHeader(value = "Host") String hostName );
+
+    @PutMapping(value = "/user/edit",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    UserOperation editUser(@RequestBody UserEntity userEntity, @RequestHeader(value = "Host") String hostName);
+
+    @DeleteMapping(value = "/user/delete",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    UserOperation deleteUser(@RequestBody UserEntity userEntity, @RequestHeader(value = "Host") String hostName);
+
+}
